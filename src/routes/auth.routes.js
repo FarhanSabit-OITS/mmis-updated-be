778a1c9a3002c4a9b7b5d3cc808bd89968f1d0a1
AzwadFawadHasan <azwad@oitsdhaka.com>
2025-12-17 12:29:16 +0600

    feat: added refresh token route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/auth.controller');
 router.post('/register', authController.register);
 router.get('/verify-email', authController.verifyEmail);
 router.post('/login', authController.login);
-router.post('/resend-verification', authController.resendVerification);
+router.post('/refresh', authController.refresh);
+router.post('/logout', authController.logout);router.post('/resend-verification', authController.resendVerification);
 
 module.exports = router;
