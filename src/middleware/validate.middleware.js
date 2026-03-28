e4f06a3ff30bbe061120ad4bf3e5aa5c0422f4dc
thanwin84 <thanwinhline84@gmail.com>
2026-03-29 02:56:38 +0600

    feat: add validation middleware using Zod for request validation

---
+++ src/middleware/validate.middleware.js
@@ -0,0 +1,17 @@
+import { ZodError } from "zod";
+
+export const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    });
+    next();
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return res.status(400).json({ errors: err.errors });
+    }
+    next(err);
+  }
+};
