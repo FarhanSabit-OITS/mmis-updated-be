91afded9dc673ec85f357bec5511954541011a8e
thanwin84 <thanwinhline84@gmail.com>
2026-04-01 23:22:04 +0600

    refactor: Remove unnecessary indexes to improve write performance

---
+++ prisma/migrations/20260401171814_remove_unnecessary_indexes_for_write_performance/migration.sql
@@ -0,0 +1,188 @@
+-- DropIndex
+DROP INDEX "cities_cityType_idx";
+
+-- DropIndex
+DROP INDEX "cities_code_idx";
+
+-- DropIndex
+DROP INDEX "customers_customerCode_idx";
+
+-- DropIndex
+DROP INDEX "customers_customerType_idx";
+
+-- DropIndex
+DROP INDEX "customers_loyaltyPoints_idx";
+
+-- DropIndex
+DROP INDEX "districts_code_idx";
+
+-- DropIndex
+DROP INDEX "districts_districtType_idx";
+
+-- DropIndex
+DROP INDEX "geolocations_country_idx";
+
+-- DropIndex
+DROP INDEX "geolocations_regionType_idx";
+
+-- DropIndex
+DROP INDEX "guests_expiryDate_idx";
+
+-- DropIndex
+DROP INDEX "guests_guestCode_idx";
+
+-- DropIndex
+DROP INDEX "guests_sponsorId_idx";
+
+-- DropIndex
+DROP INDEX "market_aisles_aisleNumber_idx";
+
+-- DropIndex
+DROP INDEX "market_aisles_aisleType_idx";
+
+-- DropIndex
+DROP INDEX "market_aisles_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "market_authorities_authorityName_idx";
+
+-- DropIndex
+DROP INDEX "market_authorities_registrationNumber_idx";
+
+-- DropIndex
+DROP INDEX "market_levels_levelNumber_idx";
+
+-- DropIndex
+DROP INDEX "market_levels_status_idx";
+
+-- DropIndex
+DROP INDEX "market_levels_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "market_sections_levelId_idx";
+
+-- DropIndex
+DROP INDEX "market_sections_sectionType_idx";
+
+-- DropIndex
+DROP INDEX "market_sections_supervisorId_idx";
+
+-- DropIndex
+DROP INDEX "market_sections_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "markets_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "markets_marketType_idx";
+
+-- DropIndex
+DROP INDEX "markets_status_idx";
+
+-- DropIndex
+DROP INDEX "markets_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "members_membershipNumber_idx";
+
+-- DropIndex
+DROP INDEX "members_registrationNumber_idx";
+
+-- DropIndex
+DROP INDEX "shops_occupationStatus_idx";
+
+-- DropIndex
+DROP INDEX "shops_shopType_idx";
+
+-- DropIndex
+DROP INDEX "shops_status_idx";
+
+-- DropIndex
+DROP INDEX "shops_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "stakeholders_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "stakeholders_kycStatus_idx";
+
+-- DropIndex
+DROP INDEX "stakeholders_stakeholderType_idx";
+
+-- DropIndex
+DROP INDEX "stalls_category_idx";
+
+-- DropIndex
+DROP INDEX "stalls_stallType_idx";
+
+-- DropIndex
+DROP INDEX "stalls_status_idx";
+
+-- DropIndex
+DROP INDEX "stalls_uniqueCode_idx";
+
+-- DropIndex
+DROP INDEX "suppliers_supplierCode_idx";
+
+-- DropIndex
+DROP INDEX "suppliers_supplierType_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_country_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_dateOfBirth_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_gender_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_nationalId_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_passportNumber_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_personalQRCode_idx";
+
+-- DropIndex
+DROP INDEX "user_profiles_taxIdNumber_idx";
+
+-- DropIndex
+DROP INDEX "user_sessions_deviceId_idx";
+
+-- DropIndex
+DROP INDEX "user_sessions_isActive_idx";
+
+-- DropIndex
+DROP INDEX "users_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "users_emailVerified_idx";
+
+-- DropIndex
+DROP INDEX "users_lastLogin_idx";
+
+-- DropIndex
+DROP INDEX "users_phoneVerified_idx";
+
+-- DropIndex
+DROP INDEX "vendors_businessLicenseNumber_idx";
+
+-- DropIndex
+DROP INDEX "vendors_primaryMarketId_idx";
+
+-- DropIndex
+DROP INDEX "vendors_vendorCode_idx";
+
+-- DropIndex
+DROP INDEX "verification_tokens_email_idx";
+
+-- DropIndex
+DROP INDEX "verification_tokens_isUsed_idx";
+
+-- DropIndex
+DROP INDEX "verification_tokens_phone_idx";
+
+-- DropIndex
+DROP INDEX "verification_tokens_tokenType_idx";
