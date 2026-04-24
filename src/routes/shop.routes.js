2419f1aca1692e1a061db906437a118906a8a6ac
thanwin97 <win@oitsdhaka.com>
2026-04-24 09:10:32 +0600

    feat: implement shop creation and deletion functionality with validation

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { validate } = require("../middleware/validate.middleware");
-const { getShopsQuerySchema } = require("../validations/shop.validation");
+const { getShopsQuerySchema, createShopSchema } = require("../validations/shop.validation");
 const shopController = require("../controllers/shop.controller");
 
+router.post("/", validate(createShopSchema), shopController.createShop);
 router.get("/", validate(getShopsQuerySchema), shopController.getShops);
 router.get("/:id", shopController.getShopDetails);
 router.put("/:id", shopController.editShop);
+router.delete('/:id', shopController.deleteShop);
 
 module.exports = router;
