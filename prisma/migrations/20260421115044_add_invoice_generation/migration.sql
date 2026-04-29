9e49e59269292b625d88dd3686d8e70148b5c128
FarhanSabit-OITS <sabit@oitsdhaka.com>
2026-04-29 11:48:10 +0600

    fix: apply upstream migration hotfix (DROP COLUMN IF EXISTS mmisId)

---
+++ prisma/migrations/20260421115044_add_invoice_generation/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dismissedAt` on the `notifications` table. All the data in the column will be lost.
+  - You are about to drop the column `marketId` on the `notifications` table. All the data in the column will be lost.
+  - You are about to drop the column `requiresDismissal` on the `notifications` table. All the data in the column will be lost.
+  - You are about to drop the column `targetRole` on the `notifications` table. All the data in the column will be lost.
+  - You are about to drop the column `mmisId` on the `user_profiles` table. All the data in the column will be lost.
+  - Made the column `userId` on table `notifications` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "invitations" DROP CONSTRAINT IF EXISTS "invitations_roleId_fkey";
+ALTER TABLE "notifications" DROP CONSTRAINT IF EXISTS "notifications_marketId_fkey";
+DROP INDEX IF EXISTS "notifications_marketId_idx";
+DROP INDEX IF EXISTS "user_profiles_mmisId_key";
+
+-- AlterTable
+ALTER TABLE "notifications"
+DROP COLUMN IF EXISTS "dismissedAt",
+DROP COLUMN IF EXISTS "marketId",
+DROP COLUMN IF EXISTS "requiresDismissal",
+DROP COLUMN IF EXISTS "targetRole",
+ALTER COLUMN "userId" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "user_profiles" DROP COLUMN IF EXISTS "mmisId";
